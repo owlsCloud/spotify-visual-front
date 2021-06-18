81135d3d5bc67088a5e16e6611287cec3ec0f55a
Gerardo Garza <owlscloud@gmail.com>
2021-06-17 20:08:58 -0500

    a

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200;300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
